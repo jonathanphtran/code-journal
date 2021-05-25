d34a6faa1fdc1950c7e1437ddda8101e6924ffc1
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-25 11:33:17 -0700

    Added in a local storage check.

--- js/data.js
+++ js/data.js
@@ -6,6 +6,12 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+var localData = localStorage.getItem('data');
+
+if (localData !== null) {
+  localData = JSON.parse(localData);
+  data = localData;
+}
 
 window.addEventListener('beforeunload', function (event) {
   var newData = JSON.stringify(data);
