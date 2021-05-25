0478eeab35f2224ae3469503149973fd7c251f39
Jonathan Tran <jonathanphtran@gmail.com>
2021-05-24 17:29:27 -0700

    Increment the nextEntryId on the data model.

--- js/main.js
+++ js/main.js
@@ -3,10 +3,32 @@
 
 var $img = document.querySelector('img');
 var $form = document.querySelector('form');
+var $photoURL = document.querySelector('#photo-URL');
 
-$form.addEventListener('submit', changeImage);
+var entryInfo = {};
+
+$photoURL.addEventListener('input', changeImage);
+
 function changeImage() {
   event.preventDefault();
   var newImg = $form[1].value;
   $img.setAttribute('src', newImg);
+}
+
+$form.addEventListener('submit', addJournalToObj);
+
+function addJournalToObj() {
+  event.preventDefault();
+
+  var newTitle = $form[0].value;
+  var newImg = $form[1].value;
+  var newNotes = $form[2].value;
+
+  entryInfo.title = newTitle;
+  entryInfo.image = newImg;
+  entryInfo.notes = newNotes;
+  entryInfo.entryID = data.nextEntryId;
+
+  data.nextEntryId++;
+
 }
